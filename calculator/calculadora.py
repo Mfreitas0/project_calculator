8793977ab1e758c4e129113a4f8fcb3cd90d46b0
Mfreitas0 <mfreitasc0@gmail.com>
2023-11-13 10:09:51 -0300

    feat: att. estilo calculadora

--- calculator/calculadora.py
+++ calculator/calculadora.py
@@ -8,15 +8,55 @@ class   Calculadora(QMainWindow):
         self.setWindowTitle('Calculadora')
         self.setFixedSize(400, 400)
         self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
         
         self.display = QLineEdit()
-        self.grid = QGridLayout(self.cw)
         self.grid.addWidget(self.display, 0, 0,1, 5)
+        self.display.setDisabled(True)
+        self.display.setStyleSheet(
+            '* {background: white; color: #000; front-size: 30px;}'
+        )
+        self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.add_btn(QPushButton('7'), 1, 0, 1,1)
+        self.add_btn(QPushButton('8'), 1, 1, 1,1)
+        self.add_btn(QPushButton('9'), 1, 2, 1,1)
+        self.add_btn(QPushButton('+'), 1, 3, 1,1)
+        self.add_btn(QPushButton('='), 1, 4, 1,1)
+        
+        self.add_btn(QPushButton('4'), 2, 0, 1,1)
+        self.add_btn(QPushButton('5'), 2, 1, 1,1)
+        self.add_btn(QPushButton('6'), 2, 2, 1,1)
+        self.add_btn(QPushButton('-'), 2, 3, 1,1)
+        self.add_btn(QPushButton('C'), 2, 4, 1,1)
+        
+        self.add_btn(QPushButton('1'), 3, 0, 1,1)
+        self.add_btn(QPushButton('2'), 3, 1, 1,1)
+        self.add_btn(QPushButton('3'), 3, 2, 1,1)
+        self.add_btn(QPushButton('x'), 3, 3, 1,1)
+        self.add_btn(QPushButton('<-'), 3, 4, 1,1)
+        
+        self.add_btn(QPushButton('.'), 4, 0, 1,1)
+        self.add_btn(QPushButton('0'), 4, 1, 1,1)
+        self.add_btn(QPushButton('()'), 4, 2, 1,1)
+        self.add_btn(QPushButton('/'), 4, 3, 1,1)
+        self.add_btn(QPushButton('%'), 4, 4, 1,1)
         
         self.setCentralWidget(self.cw)
         
+    def add_btn(self, btn, row, col, rowspan, colspan, funcao=None):
+        if not funcao:
+            self.grid.addWidget(btn, row, col, rowspan, colspan)
+            btn.clicked.connect(
+                lambda: self.display.setText(
+                    self.display.text() + btn.text()
+                )
+            )
+        else:
+            btn.clicked(funcao)
+        btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+            
 if __name__ == '__main__':
-        qt = QApplication(sys.argv)
-        calc = Calculadora()
-        calc.show()
-        qt.exec_()
+    qt = QApplication(sys.argv)
+    calc = Calculadora()
+    calc.show()
+    qt.exec_()
