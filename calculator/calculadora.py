e7c7b419bf56c0e17d0e6fca4242d412ec1ff2a9
Mfreitas0 <mfreitasc0@gmail.com>
2023-11-18 09:15:19 -0300

    feat: Logica Calculadora.

--- calculator/calculadora.py
+++ calculator/calculadora.py
@@ -20,41 +20,58 @@ class   Calculadora(QMainWindow):
         self.add_btn(QPushButton('7'), 1, 0, 1,1)
         self.add_btn(QPushButton('8'), 1, 1, 1,1)
         self.add_btn(QPushButton('9'), 1, 2, 1,1)
-        self.add_btn(QPushButton('+'), 1, 3, 1,1)
-        self.add_btn(QPushButton('='), 1, 4, 1,1)
+        self.add_btn(
+            QPushButton('C'), 1, 3, 1,1,
+                    lambda: self.display.setText(''))
+        self.add_btn(
+            QPushButton('='), 1, 4, 1,1,
+            self.equals)
         
         self.add_btn(QPushButton('4'), 2, 0, 1,1)
         self.add_btn(QPushButton('5'), 2, 1, 1,1)
         self.add_btn(QPushButton('6'), 2, 2, 1,1)
-        self.add_btn(QPushButton('-'), 2, 3, 1,1)
-        self.add_btn(QPushButton('C'), 2, 4, 1,1)
+        self.add_btn(
+            QPushButton('<-'), 2, 3, 1,1,
+            lambda: self.display.setText(
+                self.display.text()[:-1]
+            ))
+        self.add_btn(QPushButton('%'), 2, 4, 1,1)
         
         self.add_btn(QPushButton('1'), 3, 0, 1,1)
         self.add_btn(QPushButton('2'), 3, 1, 1,1)
         self.add_btn(QPushButton('3'), 3, 2, 1,1)
-        self.add_btn(QPushButton('x'), 3, 3, 1,1)
-        self.add_btn(QPushButton('<-'), 3, 4, 1,1)
+        self.add_btn(QPushButton('+'), 3, 3, 1,1)
+        self.add_btn(QPushButton('-'), 3, 4, 1,1)
         
         self.add_btn(QPushButton('.'), 4, 0, 1,1)
         self.add_btn(QPushButton('0'), 4, 1, 1,1)
-        self.add_btn(QPushButton('()'), 4, 2, 1,1)
-        self.add_btn(QPushButton('/'), 4, 3, 1,1)
-        self.add_btn(QPushButton('%'), 4, 4, 1,1)
+        self.add_btn(QPushButton(' '), 4, 2, 1,1)
+        self.add_btn(QPushButton('*'), 4, 3, 1,1)
+        self.add_btn(QPushButton('/'), 4, 4, 1,1)
         
         self.setCentralWidget(self.cw)
         
     def add_btn(self, btn, row, col, rowspan, colspan, funcao=None):
+        self.grid.addWidget(btn, row, col, rowspan, colspan)
         if not funcao:
-            self.grid.addWidget(btn, row, col, rowspan, colspan)
             btn.clicked.connect(
                 lambda: self.display.setText(
                     self.display.text() + btn.text()
                 )
             )
         else:
-            btn.clicked(funcao)
+            btn.clicked.connect(funcao)
         btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-            
+    def equals(self):
+        try:
+            self.display.setText(
+                str(eval(self.display.text()))
+            )
+        except Exception as e:
+            self.display.setText('Conta Inválida')
+    
+    
+    
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
     calc = Calculadora()
